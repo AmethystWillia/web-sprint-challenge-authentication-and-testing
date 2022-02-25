464daeec360a159d66d3f5fad11eec467f021277
Amethyst Willia <zmithic@gmail.com>
2022-02-25 13:11:52 -0600

    Set up server test skeleton

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
-// Write your tests here
+const db = require('../data/dbConfig');
+
+const request = require('supertest');
+const server = require('./server');
+
+// -------------------------- //
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db('users').truncate();
+});
+
+// -------------------------- //
+
 test('sanity', () => {
-  expect(true).toBe(false)
-})
+  expect(true).toBe(false);
+});
