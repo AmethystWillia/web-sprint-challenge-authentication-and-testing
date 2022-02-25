b35362c0895e7a5097354db003317c9583e9fb7e
Amethyst Willia <zmithic@gmail.com>
2022-02-25 13:33:27 -0600

    Passed insert test

---
+++ api/users/users-model.js
@@ -0,0 +1,44 @@
+const db = require('../../data/dbConfig');
+
+const getAll = () => {
+    return db('users');
+};
+
+const getById = (id) => {
+    return db('users')
+        .where('id', id)
+        .first();
+};
+
+const insert = async (user) => {
+    const [id] = await db('users')
+        .insert(user);
+    
+        return getById(id);
+};
+
+const update = async (id, changes) => {
+    await db('users')
+        .update({ username: changes.username, password: changes.password })
+        .where('id', id);
+    
+    return getById(id);
+};
+
+const remove = async (id) => {
+    const result = await getById(id);
+
+    await db('users')
+        .where('id', id)
+        .del();
+
+    return result;
+};
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+    update,
+    remove,
+};
